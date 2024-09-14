25e98d9c1256244d7d2fa8191cdbf577b23db08c
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-09-14 21:49:13 +0600

    create post slice

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+//at first write initialState
+const initialState = {
+    posts: [],
+    isLoading: false,
+    isError: false,
+    error: null,
+};
+
+
+//2nd write component
+const postSlice=createSlice({
+
+})
+
+//3rd step writing
+export default postSlice.reducer
